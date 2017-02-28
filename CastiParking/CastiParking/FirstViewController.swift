bd5b29ce9c615d7d95da144b160d363549dc4b8b
Katie Mishra <katiemishra@Katies-MacBook-Air.local>
2017-02-28 11:39:48 -0800

    first commit

---
+++ CastiParking/CastiParking/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CastiParking
+//
+//  Created by Katie Mishra on 2/3/17.
+//  Copyright © 2017 CS3. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
